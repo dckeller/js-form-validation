477e63f78c6758eb9a749e85ba9ecd64608c9872
David Keller <dckeller1@gmail.com>
2018-01-20 13:35:37 -0700

    Add email validator

--- source/form-validator.js
+++ source/form-validator.js
@@ -1,5 +1,21 @@
 // shorthand for $(document).ready();
-$(function(){
-  //Your code...
+$(document).ready(function(){
 
+  $("form").on("submit", function(e) {
+  	e.preventDefault();
+  	var email = document.getElementById("emailID").value;
+  	var password = document.getElementById("passwordID").value;
+  		
+// check if there is an @ in the email using RegExp //
+  		if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+  			return true
+  		}
+
+  		else if
+  		else {
+  			$("#errors").append("<b>-Invalid Email Address</b>")
+  		}
+
+
+  });
 });
