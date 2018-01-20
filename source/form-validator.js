270e3d17a77b11a1bcc0f5be092c0f71f6a16940
David Keller <dckeller1@gmail.com>
2018-01-20 13:59:30 -0700

    Message block display

--- source/form-validator.js
+++ source/form-validator.js
@@ -10,12 +10,16 @@ $(document).ready(function(){
   		if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
   			return true
   		}
-
-  		else if
   		else {
-  			$("#errors").append("<b>-Invalid Email Address</b>")
+  			$("#errors").append("<b class='message'>-Invalid Email Address</b>")
   		}
 
-
+ // check if there is a number in the password using RegExp //
+ 		if(/^(?=.*[0-9])$/.test(password)) {
+ 			return true
+ 		}
+ 		else {
+ 			$("#errors").append("<b class='message'>-Password must contain a number</b>")
+ 		}
   });
 });
